2f59dd43ed6526f292b8f2a43866fc74593aecfc
Geoff R <70560312+LetTheWookieWin92@users.noreply.github.com>
2021-06-27 19:50:20 +1000

    text button done

--- src/components/examples/buttonExample.jsx
+++ src/components/examples/buttonExample.jsx
@@ -12,15 +12,23 @@ const useStyles = theme => ({
 	}
 });
 
+const textButtonDescriptors = ["great", "good", "fine", "bad", "awful"];
+const textButtonConnector = ["are", "aren't"];
+const textButtonSubjects = ["Apples", "Carrots", "Blueberries"];
+
+
+
 class ButtonExample extends Component {
 	state = { 
 		containedButtonText: "Press a button",
 		containedButtonPresses: 1,
-		containedButtonType: "none"
+		containedButtonType: "none",
+
+		textButtonText: "Press a button",
+		textButtonTextState: [0,0,0]
  }
 
 	containedButtonPressed(button) {
-
 		if(button === this.state.containedButtonType)
 		{
 			let containedButtonPresses = this.state.containedButtonPresses +1;
@@ -34,8 +42,29 @@ class ButtonExample extends Component {
 			let containedButtonText = button + " button has been pressed " + containedButtonPresses + " times";
 			this.setState({containedButtonPresses, containedButtonText, containedButtonType});
 		}
+	}
 
-		
+	textButtonPressed(button) {
+
+		let textButtonTextState = this.state.textButtonTextState;
+		let textButtonText = "";
+
+		if(button === "Default")
+		{
+			textButtonTextState[0] = (textButtonTextState[0] == 2) ? 0 : ++textButtonTextState[0];
+		}
+		else if(button === "Primary")
+		{
+			textButtonTextState[1] = (textButtonTextState[1] == 1) ? 0 : ++textButtonTextState[1];
+		}
+		else
+		{
+			textButtonTextState[2] = (textButtonTextState[2] == 4) ? 0 : ++textButtonTextState[2];
+		}
+
+		textButtonText = textButtonSubjects[textButtonTextState[0]] + " " + textButtonConnector[textButtonTextState[1]] + " " + textButtonDescriptors[textButtonTextState[2]];
+
+		this.setState({textButtonText, textButtonTextState});
 	}
 
 	render() { 
@@ -53,7 +82,17 @@ class ButtonExample extends Component {
 					</React.Fragment>
 					
 				}/>
-				<ExampleSection title="Text buttons" description="Used for lower emphasis actions, such as those found in dialogues or cards. Making use of these helps maintain the emphasis of the primary content" content=""/>
+				<ExampleSection title="Text buttons" description="Used for lower emphasis actions, such as those found in dialogues or cards. Making use of these helps maintain the emphasis of the primary content" content=
+				{
+					<React.Fragment>
+						<p>{this.state.textButtonText}</p>
+						<Button className={classes.buttonSpacing} onClick={ () => this.textButtonPressed("Default")}>Default</Button>
+						<Button className={classes.buttonSpacing} onClick={ () => this.textButtonPressed("Primary")} color="primary">Primary</Button>
+						<Button className={classes.buttonSpacing} onClick={ () => this.textButtonPressed("Secondary")} color="secondary">Secondary</Button>
+						<Button className={classes.buttonSpacing} onClick={ () => this.textButtonPressed("Disabled")} disabled>Disabled</Button>
+					</React.Fragment>
+					
+				}/>
 
 			</React.Fragment>
 		 );
