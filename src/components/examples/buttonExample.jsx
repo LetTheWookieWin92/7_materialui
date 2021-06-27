9715225979e0ab51d9963b104654a1c3cc126eb0
Geoff R <70560312+LetTheWookieWin92@users.noreply.github.com>
2021-06-27 14:46:44 +1000

    contained button done

--- src/components/examples/buttonExample.jsx
+++ src/components/examples/buttonExample.jsx
@@ -13,26 +13,49 @@ const useStyles = theme => ({
 });
 
 class ButtonExample extends Component {
-	state = { containedButtonText: "Press a button" }
+	state = { 
+		containedButtonText: "Press a button",
+		containedButtonPresses: 1,
+		containedButtonType: "none"
+ }
+
+	containedButtonPressed(button) {
+
+		if(button === this.state.containedButtonType)
+		{
+			let containedButtonPresses = this.state.containedButtonPresses +1;
+			let containedButtonText = button + " button has been pressed " + containedButtonPresses + " times";
+			this.setState({containedButtonPresses, containedButtonText});
+		}
+		else
+		{
+			let containedButtonType = button;
+			let containedButtonPresses = 1;
+			let containedButtonText = button + " button has been pressed " + containedButtonPresses + " times";
+			this.setState({containedButtonPresses, containedButtonText, containedButtonType});
+		}
+
+		
+	}
 
 	render() { 
 		const { classes } = this.props;
 		return ( 
 			<React.Fragment>
-			
-			<ExampleSection title="Contained buttons" description="Used for high emphasis, primary actions within the app." content=
-			{
-				<React.Fragment>
-					<Button variant="contained" className={classes.buttonSpacing}>Default</Button>
-					<Button variant="contained" className={classes.buttonSpacing} color="primary">Primary</Button>
-					<Button variant="contained" className={classes.buttonSpacing} color="secondary">Secondary</Button>
-					<Button variant="contained" className={classes.buttonSpacing} disabled>Disabled</Button>
-				</React.Fragment>
-				
-			}/>
-			<ExampleSection title="Text buttons" description="Used for lower emphasis actions, such as those found in dialogues or cards. Making use of these helps maintain the emphasis of the primary content" content=""/>
+				<ExampleSection title="Contained buttons" description="Used for high emphasis, primary actions within the app." content=
+				{
+					<React.Fragment>
+						<p>{this.state.containedButtonText}</p>
+						<Button variant="contained" className={classes.buttonSpacing} onClick={ () => this.containedButtonPressed("Default")}>Default</Button>
+						<Button variant="contained" className={classes.buttonSpacing} onClick={ () => this.containedButtonPressed("Primary")} color="primary">Primary</Button>
+						<Button variant="contained" className={classes.buttonSpacing} onClick={ () => this.containedButtonPressed("Secondary")} color="secondary">Secondary</Button>
+						<Button variant="contained" className={classes.buttonSpacing} onClick={ () => this.containedButtonPressed("Disabled")} disabled>Disabled</Button>
+					</React.Fragment>
+					
+				}/>
+				<ExampleSection title="Text buttons" description="Used for lower emphasis actions, such as those found in dialogues or cards. Making use of these helps maintain the emphasis of the primary content" content=""/>
 
-		</React.Fragment>
+			</React.Fragment>
 		 );
 	}
 }
