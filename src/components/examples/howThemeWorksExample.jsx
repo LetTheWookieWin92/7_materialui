31274d6b4dcd0ddcc2bd177d400784f3453bf41e
Geoff R <70560312+LetTheWookieWin92@users.noreply.github.com>
2021-07-07 14:52:44 +1000

    steps 1 and 2

--- src/components/examples/howThemeWorksExample.jsx
+++ src/components/examples/howThemeWorksExample.jsx
@@ -3,25 +3,54 @@ import React from 'react';
 // Components
 import NavigationBar from "../navigationBar";
 import ExampleSection from './exampleSection';
+import Grid from "@material-ui/core/Grid";
 
+//Style
+import { makeStyles } from '@material-ui/styles';
+
+//Img
+import colorTool from './../../img/ColorTool.PNG';
+import colourPicker from './../../img/ColourPicker.PNG';
+import themeComponent from './../../img/ThemeComponent.PNG';
+
+const useStyles = makeStyles(theme => ({
+    imgFitting: {
+        alignSelf: "center",
+        maxWidth: "calc(100% - 20px)",
+        margin: 10,
+    }
+}));
 
 const HowThemeWorksExample = ({title, onBackPress}) => {
+    const classes = useStyles();
     return ( 
         <React.Fragment>
             <NavigationBar textLabel={title} showBack="Yes" backPressed={onBackPress}/>
-            <ExampleSection title="1. Decide on your colour palette" description="dfsdf " content=
+            <ExampleSection title="1. Decide on your colour palette" description="Use the Material Design colour picker to select your primary and secondary colours, then use the color tool to preview and check accessibility." content=
             {
-                <React.Fragment>
-                    
-                </React.Fragment>
-                
+                <Grid container spacing={0} alignItems="center">
+                    <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
+                        <img src={colourPicker} className={classes.imgFitting} alt="Content Missing"/>
+                        <p>Colour picker</p>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={8} lg={8} xl={8}>
+                        <img src={colorTool} className={classes.imgFitting} alt="Content Missing"/>
+                        <p>Color tool</p>
+                    </Grid>
+                </Grid>
             }/>
-            <ExampleSection title="2. Create the theme component" description=" " content=
+            <ExampleSection title="2. Create the theme component" description="Using the colour palette you decided in step 1, create a new theme.jsx component." content=
             {
-                <React.Fragment>
-                    
-                </React.Fragment>
-                
+                <Grid container spacing={0} >
+                    <Grid item xs={12} sm={12} md={6} lg={6} xl={6} alignItems="center">
+                        <img src={themeComponent} className={classes.imgFitting} alt="Content Missing"/>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+                        <p className="displayBoxText">You can define primary, secondary, error, warning, info and success palette colours. Each has main, light, dark and contrastText values but if you omit light, dark and contrastText then these will be calculated from the main value.</p>
+                        <p className="displayBoxText">Some components, such as Button, can't access all of the defined palette colours. In that case, you should define a custom style and set the properties equal to those defined in the theme colour palette (see step 4).</p>
+                        <p className="displayBoxText">Import the theme component wherever you intend to implement the ThemeProvider (see step 3).</p>
+                    </Grid>
+                </Grid>
             }/>
             <ExampleSection title="3. Implement the ThemeProvider" description=" " content=
             {
